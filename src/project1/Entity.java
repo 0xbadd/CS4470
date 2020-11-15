94321a382931420ba6ea8bce6730b4947a70712c
jjeon36219 <47404324+jjeon36219@users.noreply.github.com>
2020-11-14 19:22:42 -0800

    Update Entity.java

--- src/project1/Entity.java
+++ src/project1/Entity.java
@@ -2,6 +2,16 @@ package project1;
 
 public abstract class Entity
 {
+   {
+	protected final int NOT_CONNECTED = 999;
+	public Entity(){
+		
+		for(int to = 0; to < distanceTable.length; to++){
+			for(int via = 0; via < distanceTable.length; via++){
+				distanceTable[to][via] = NOT_CONNECTED;
+			}
+		}
+	}
     // Each entity will have a distance table
     protected int[][] distanceTable = new int[NetworkSimulator.NUMENTITIES]
                                            [NetworkSimulator.NUMENTITIES];
@@ -17,5 +27,42 @@ public abstract class Entity
 
     // Print the distance table of the current entity.
     protected abstract void printDT();
+       protected int[] generateMinimumCostArray(){
+		int[] minimumCost = new int[4];
+		for(int router = 0; router < 4; router++)
+			minimumCost[router] = NOT_CONNECTED;
 
+		for(int to = 0; to < distanceTable.length; to++){
+			for(int via = 0; via < distanceTable.length; via++)
+				if(distanceTable[to][via] < minimumCost[to])
+					minimumCost[to] = distanceTable[to][via];
+		}
+		return minimumCost;
+	}
+	
+	protected void sendToLayer2(int source , int destination , int[] minimumCostArray){
+		Packet p = new Packet(source , destination , minimumCostArray);
+		NetworkSimulator.toLayer2(p);
+	}
+
+	protected boolean performUpdate(Packet p){
+		boolean changed = false;
+		int source = p.getSource();
+		for(int router = 0; router < 4; router++){
+			if(distanceTable[router][source] > p.getMincost(router) + distanceTable[source][source]){
+				changed = true;
+				distanceTable[router][source] = p.getMincost(router) + distanceTable[source][source];
+			}
+		}
+		return changed;
+	}
+
+	protected void printMinimumPaths(int[] paths){
+		 for(int path: paths){
+			 System.out.print(path + " ");
+		 }
+		 System.out.println();
+	}
 }
+
+
